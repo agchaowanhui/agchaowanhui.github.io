723c8c825064db66303f3cdba8fb51cbb04f12b1
sq <1223998091@qq.com>
2022-04-19 23:03:33 +0800

    'playerController'

---
+++ save.js
@@ -0,0 +1,18 @@
+function fake_click(obj) {
+    var ev = document.createEvent("MouseEvents");
+    ev.initMouseEvent(
+        "click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null
+    );
+    obj.dispatchEvent(ev);
+}
+
+function download(name, data) {
+    var urlObject = window.URL || window.webkitURL || window;
+
+    var downloadData = new Blob([data]);
+
+    var save_link = document.createElementNS("http://www.w3.org/1999/xhtml", "a")
+    save_link.href = urlObject.createObjectURL(downloadData);
+    save_link.download = name;
+    fake_click(save_link);
+}
